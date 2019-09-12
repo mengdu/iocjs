c13ad3b908f91acf4ecac3d89a75047f54286917
ygl <lanyueos@qq.com>
2019-09-12 11:40:36 +0800

    添加例子

--- examples/demo.ts
+++ examples/demo.ts
@@ -2,6 +2,15 @@ import { Container, Inject, Injectable } from '../src'
 
 const container = new Container()
 
+interface ConfigType {
+  port: number,
+  card?: number
+}
+
+const config: ConfigType = {
+  port: 3000,
+  card: 1001
+}
 class Engine {
   start () {
     console.log('start Engine')
@@ -14,10 +23,13 @@ class Driver {
   }
 }
 
-@Injectable()
+@Injectable('test')
 class Cat {
   @Inject()
   readonly engine: Engine;
+
+  @Inject('CONFIG')
+  readonly config: ConfigType;
 
   constructor (public driver: Driver) {}
 
@@ -29,10 +41,22 @@ class Cat {
 container.set(Engine)
 container.set(Driver)
 container.set(Cat)
+// container.set('test', Cat)
+container.set('CONFIG', config)
 
 const cat = container.get(Cat)
 
 console.log(cat)
+console.log(container)
 
 cat.start()
 cat.driver.say()
+
+const cat1 = container.get<Cat>('test')
+const cat2 = container.get<Cat>('test')
+
+console.log(cat1, cat === container.get(Cat), cat2 === cat, cat1 === cat2)
+
+const conf = container.get<ConfigType>('CONFIG')
+
+console.log(conf.port)

18931446a7ae823a9f5324be50421ce395f53850
ygl <lanyueos@qq.com>
2019-09-12 11:39:13 +0800

    键 any 类型

--- src/decorators/inject.decorator.ts
+++ src/decorators/inject.decorator.ts
@@ -1,6 +1,6 @@
 import { setPropertyTypeMap, getPropertyTypeMap, getPropertyType } from '../meta'
 
-export default function Inject (id?: string): PropertyDecorator {
+export default function Inject (id?: any): PropertyDecorator {
   return function (target: any, property: string): any {
     // 获取注入属性的类型
     const propertyType = getPropertyType(target, property)
